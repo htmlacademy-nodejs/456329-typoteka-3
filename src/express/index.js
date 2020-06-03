45dda1eeea67e12db3487dc24f88442a4b79c110
Denis Goncearuc <fristyr.dev@gmail.com>
2020-06-03 09:38:37 +0200

    Change router names

--- src/express/index.js
+++ src/express/index.js
@@ -9,10 +9,12 @@ const {
 // Маршруты приложения мы опишем в отдельных файлах.
 // Для определения маршрутов мы воспользуемся Router().
 // Примеры маршрутов будут продемонстрированы ниже по тексту.
-const categorieRoutes = require(`./routes/category-routes`);
-const articleRoutes = require(`./routes/article-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const mainRoutes = require(`./routes/main-routes`);
+const { 
+  categorieRoutes, 
+  articleRoutes, 
+  postRoutes, 
+  actionRoutes,
+} = require(`./routes/index`)
 
 // Зафиксируем порт для сервера
 const DEFAULT_PORT = 8080;
@@ -22,8 +24,8 @@ const app = express();
 // Подключим созданные маршруты
 app.use(`/categories`, categorieRoutes);
 app.use(`/articles`, articleRoutes);
-app.use(`/my`, myRoutes);
-app.use(`/`, mainRoutes);
+app.use(`/posts`, postRoutes);
+app.use(`/`, actionRoutes);
 
 // Запуск сервера
 app.listen(DEFAULT_PORT, () => logInfo(`Сервер запущен на порту: ${DEFAULT_PORT}`, `green`));
