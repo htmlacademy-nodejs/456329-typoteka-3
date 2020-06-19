efd2fc4791bd774a9e16294e7b6659287bc0665f
Denis Goncearuc <fristyr.dev@gmail.com>
2020-06-19 11:11:14 +0200

    Add e2e tests

--- src/express/index.js
+++ src/express/index.js
@@ -26,6 +26,20 @@ app.use(express.json());
 // Используем REST api
 app.use(API_PREFIX, data);
 
+app.use((req, res) => {
+  res.status(404)
+  res.json({
+    error: {
+      'name': 'Error',
+      'status': 404,
+      'message': 'Invalid Request',
+      'statusCode': 404,
+    }
+  });
+  //console.error(`Wrong route`);
+});
+
+
 // Подключим созданные маршруты
 app.use(`/categories`, categorieRoutes);
 app.use(`/articles`, articleRoutes);
@@ -39,3 +53,5 @@ app.set(`view engine`, `pug`);
 
 // Запуск сервера
 app.listen(DEFAULT_PORT, () => logInfo(`Сервер запущен на порту: ${DEFAULT_PORT}`, `green`));
+
+module.exports = app;
